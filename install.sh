a0426d29e419c11d7a2784ab68caadd6e015b761
zivmax <zivmax@foxmail.com>
2024-09-13 22:37:19 +0800

    delete

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 rm -rf $HOME/.local/bin/igem-upload
 rm -rf $HOME/.local/bin/igem
-cp main.py $HOME/.local/bin/igem-upload
+pyinstaller --name igem-upload --onefile main.py
+cp ./dist/igem-upload $HOME/.local/bin/igem-upload
 mkdir $HOME/.local/bin/igem
 cp config.json $HOME/.local/bin/igem/config.json
-cp src/uploads.py $HOME/.local/bin/igem/uploads.py
 chmod +x $HOME/.local/bin/igem-upload
 echo "iGEM Upload installed successfully!"
 echo "Please restart your terminal."
--- main.py
+++ main.py
@@ -1,42 +1,42 @@
-#!/usr/bin/env python
 import os
-import sys
 import argparse
 import json
+from src.uploads import Session
 
-if __name__ == "__main__":
-    script_name = os.path.basename(sys.argv[0])
-
-    if script_name == "main.py":
-        from src.uploads import Session
+def delete(client: Session, remote_path: str) -> None:
+    if os.path.isfile(remote_path):
+        client.delete(remote_path)
     else:
-        from igem.uploads import Session
-
-
-def delete_file_or_dir(client: Session, remote_dir: str) -> None:
-    if os.path.isfile(remote_dir):
-        client.delete(remote_dir)
-    else:
-        client.delete_dir(remote_dir)
+        client.delete_dir(remote_path, True)
 
 
 def sync_work_dir(client: Session, local_work_dir: str, remote_work_dir: str) -> None:
     """Upload all local files to remote without overwriting check, then download all remote files to local."""
     client.upload_dir(local_work_dir, remote_work_dir)
-    client.download_dir(remote_work_dir, local_work_dir)
+    client.download_dir(remote_work_dir, True)
 
 
-def download_remote_dir(client: Session, remote_dir: str, local_dir: str) -> None:
-    """Download local directory to remote directory without overwriting check."""
-    client.download_dir(remote_dir, local_dir)
+def download(client: Session, remote_path: str) -> None:
+    """Download from remote without overwriting check."""
+    # If it's a file
+    if os.path.isfile(remote_path):
+        client.download_file(remote_path)
+    # If it's a directory
+    else:
+        client.download_dir(remote_path, True)
 
 
-def upload_local_dir(client: Session, local_dir: str, remote_dir: str) -> None:
-    """Upload local directory to remote directory without overwriting check."""
-    client.upload_dir(local_dir, remote_dir)
+def upload(client: Session, local_path: str, remote_path: str) -> None:
+    """Upload to remote without overwriting check."""
+    # If it's a file
+    if os.path.isfile(remote_path):
+        client.upload_file(remote_path)
+    # If it's a directory
+    else:
+        client.upload_dir(local_path, remote_path, True)
 
 
-def load_config(config_path="config.json"):
+def load_config(config_path="config.json") -> dict:
     """Loads the configuration from a JSON file."""
     try:
         with open(config_path, "r") as f:
@@ -47,67 +47,72 @@ def load_config(config_path="config.json"):
         return None
 
 
-def get_default_remote_dir(local_dir):
+def get_default_remote_path(local_root):
     """Calculates the default remote directory based on the local path."""
-    return os.path.relpath(local_dir, os.getcwd())
+    return os.path.relpath(local_root, os.getcwd())
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="File and Directory Management CLI")
+    parser = argparse.ArgumentParser(
+        description="iGEM CDN File and Directory Management CLI"
+    )
     parser.add_argument(
         "action",
-        choices=["delete", "sync", "download", "upload"],
+        choices=["delete", "sync", "download", "upload", "query"],
         help="Action to perform",
     )
-    parser.add_argument("--remote-dir", help="Remote directory path")
-    parser.add_argument("--local-dir", help="Local directory path")
-    parser.add_argument(
-        "--config", default="config.json", help="Path to the configuration file"
-    )
+    parser.add_argument("-rp", "--remote-path", help="Remote path")
+    parser.add_argument("-lp", "--local-path", help="Local path")
+    parser.add_argument("--config", help="Path to the configuration file")
 
     args = parser.parse_args()
 
-    if config is None:
-        config = f"{os.getenv('HOME')}/.local/bin/igem-upload/igem/config.json"
-    config = load_config(args.config)
+    if args.config is None:
+        config = load_config(f"{os.getenv('HOME')}/.local/bin/igem/config.json")
+    else:
+        config = load_config(args.config)
 
     if config is None:
         return
 
     username = config.get("username")
     password = config.get("password")
+    local_root = config.get("local_root")
     if username is None or password is None:
         print(
             "Error: 'username' and 'password' must be specified in the configuration file."
         )
         return
 
-    # Calculate default remote_dir if not provided
-    if not args.remote_dir:
-        if args.action != "delete":  # 'delete' doesn't use local_dir
-            if not args.local_dir:
+    # Calculate default remote_path if not provided
+    if args.remote_path is None:
+        if args.action != "delete":  # 'delete' doesn't use local_path
+            if not args.local_path:
                 print(
-                    f"Error: --local-dir is required for {args.action} action when --remote-dir is not specified."
+                    f"Error: --local-path is required for {args.action} action when --remote-path is not specified."
                 )
                 return
-            args.remote_dir = get_default_remote_dir(args.local_dir)
+            args.remote_path = get_default_remote_path(local_root)
         else:
-            args.remote_dir = ""  # Default to root if not provided for delete
+            args.remote_path = ""  # Default to root if not provided for delete
 
     client = Session()
     client.login(username, password)
 
-    if args.action == "delete":
-        if not args.remote_dir:
-            print("Error: --remote-dir is required for delete action")
-            return
-        delete_file_or_dir(client, args.remote_dir)
-    elif args.action == "sync":
-        sync_work_dir(client, args.local_dir, args.remote_dir)
-    elif args.action == "download":
-        download_remote_dir(client, args.remote_dir, args.local_dir)
-    elif args.action == "upload":
-        upload_local_dir(client, args.local_dir, args.remote_dir)
+    match args.action:
+        case "delete":
+            if not args.remote_path:
+                print("Error: --remote-path is required for delete action")
+                return
+            delete(client, args.remote_path)
+        case "sync":
+            sync_work_dir(client, args.local_path, args.remote_path)
+        case "download":
+            download(client, args.remote_path)
+        case "upload":
+            upload(client, args.local_path, args.remote_path)
+        case "query":
+            client.query(args.remote_path)
 
 
 if __name__ == "__main__":
