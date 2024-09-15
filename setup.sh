b6d791ef3deadff2436e0906955a2317c29c3714
zivmax <zivmax@foxmail.com>
2024-09-15 15:24:37 +0800

    refactor: Refactor CI configurations to use setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 INSTALL_PATH="$HOME/.local/bin"
-PROGRAM_NAME="igem-upload"
+PROGRAM_NAME="igem-cdn"
 PROGRAM_DIR="$INSTALL_PATH/igem"
 
 install_program() {
-    source .venv/bin/activate
-    python -m PyInstaller main.py  --name $PROGRAM_NAME --hidden-import=_cffi_backend -F --clean --strip
     rm -rf $INSTALL_PATH/$PROGRAM_NAME
     rm -rf $PROGRAM_DIR
     cp ./dist/$PROGRAM_NAME $INSTALL_PATH/$PROGRAM_NAME
@@ -23,10 +21,23 @@ uninstall_program() {
     echo "iGEM Upload uninstalled successfully!"
 }
 
+
+build_program() {
+    if [ -d ".venv" ]; then
+        source .venv/bin/activate
+    else
+        echo "Virtual environment not found, using system packages."
+    fi
+    
+    python -m PyInstaller main.py --name $PROGRAM_NAME --hidden-import=_cffi_backend -F --clean --strip
+}
 if [ "$1" == "uninstall" ]; then
     uninstall_program
 elif [ "$1" == "install" ]; then
+    build_program
     install_program
+elif [ "$1" == "build" ]; then
+    build_program
 else
     echo "Please specify the action you want to perform: 'install' or 'uninstall'."
 fi
