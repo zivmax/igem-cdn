d9e044768416a701eb2d0cb00f41b8cdde516830
zivmax <zivmax@foxmail.com>
2024-09-21 23:20:50 +0800

    chore: update links

--- setup-remote.sh
+++ setup-remote.sh
@@ -43,9 +43,9 @@ uninstall_program() {
 get_program() {
     ISA=$1
     if [ "$ISA" == "ARM" ]; then
-        DOWNLOAD_URL="https://example.com/arm-release.zip"
+        DOWNLOAD_URL="https://gitlab.igem.org/zivmax/igem-cdn/-/jobs/924628/artifacts/download?file_type=archive"
     elif [ "$ISA" == "X86" ]; then
-        DOWNLOAD_URL="https://example.com/x86-release.zip"
+        DOWNLOAD_URL="https://github.com/zivmax/igem-cdn/releases/download/latest/release.zip"
     else
         echo "Invalid ISA specified. Please choose 'ARM' or 'X86'."
         exit 1
